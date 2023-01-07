3022fb053752b5ec0cbdad496802455325c9ab39
kanyi <107926388+jkanyi-web@users.noreply.github.com>
2023-01-07 19:07:17 +0300

    Update index.js
    Bug created

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const HAMBURGER_BUTTON = document.querySelector('.menu');
 const NAVBAR_LOGO = document.querySelector('.nam');
 const MOBILE_MENU_SCREEN = document.querySelector('.mobile-menu-screen');
 HAMBURGER_BUTTON.addEventListener('click', () => {
-  HAMBURGER_BUTTON.classList.toggle('active');
+  HAMBURGER_BUTTON.classList.toggle('.active');
   MOBILE_MENU_SCREEN.classList.toggle('active');
   NAVBAR_LOGO.classList.toggle('active');
 });
@@ -11,6 +11,6 @@ MOBILE_MENU_LINKS.forEach((element) => {
   element.addEventListener('click', () => {
     HAMBURGER_BUTTON.classList.remove('active');
     MOBILE_MENU_SCREEN.classList.remove('active');
-    NAVBAR_LOGO.classList.remove('active');
+    NAVBAR_LOGO.classList.remove('.active');
   });
 });
